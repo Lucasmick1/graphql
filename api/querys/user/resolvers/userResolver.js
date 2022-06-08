efd7ce485bac0c0c14ff6a576f1cb66c411bd35f
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-08 09:07:29 -0300

    Adicionando método de deleção no resolver do usuário

--- api/querys/user/resolvers/userResolver.js
+++ api/querys/user/resolvers/userResolver.js
@@ -1,11 +1,12 @@
 const userResolvers = {
     Query: {
         users: (root, _, { dataSources }) => dataSources.usersAPI.getUsers(),
-        user: (root, {id}, {dataSources}) => dataSources.usersAPI.getUser(id)
+        user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUser(id)
     },
     Mutation: {
-        includeUser: (root, user, {dataSources}) => dataSources.usersAPI.includeUser(user),
-        updateUser: (root, update, {dataSources}) => dataSources.usersAPI.updateUser(update)
+        includeUser: (root, user, { dataSources }) => dataSources.usersAPI.includeUser(user),
+        updateUser: (root, update, { dataSources }) => dataSources.usersAPI.updateUser(update),
+        deleteUserById: (root, { id }, { dataSources }) => dataSources.usersAPI.deleteUserById(id)
     }
 
 }
